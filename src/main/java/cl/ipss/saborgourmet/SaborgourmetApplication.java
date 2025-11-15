c5f277846e760c26bfb31b8f69ae85b473085f9b
Iván Santibáñez <iasantibanez@indracompany.com>
2025-11-14 22:11:14 -0300

    first commit

---
+++ src/main/java/cl/ipss/saborgourmet/SaborgourmetApplication.java
@@ -0,0 +1,13 @@
+package cl.ipss.saborgourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborgourmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaborgourmetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ipss/saborgourmet/SaborgourmetApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ipss.saborgourmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaborgourmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
